dcffa295b6aa1565b33e60b486e2cfbf2547bad1
Jin Yong <kangjinyong@gmail.com>
2021-10-29 22:42:41 +0800

    skeleton added

--- projects/jyk-library/src/public-api.ts
+++ projects/jyk-library/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of jyk-library
  */
 
-export * from './lib/jyk-library.service';
-export * from './lib/jyk-library.component';
-export * from './lib/jyk-library.module';
+export * from './lib/responsive-table/responsive-table.module';
